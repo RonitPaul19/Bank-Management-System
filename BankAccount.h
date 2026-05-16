ee86b97786164ac2464ee003b6c346fb01f52dc9
Ronit Paul <ronitpaulindia@gmail.com>
2026-05-17 00:21:19 +0530

    User defined header file to manage bank accounts

---
+++ BankAccount.h
@@ -0,0 +1,43 @@
+#ifndef BANK_ACCOUNT_H
+#define BANK_ACCOUNT_H
+
+#include <string>
+
+class BankAccount {
+ private:
+  int accountNumber;
+  std::string accountName;
+  double balance;
+
+  inline static int nextAccountNumber = 1001;
+
+ public:
+  enum class DepositStatus { SUCCESS, INVALID_AMOUNT };
+  enum class WithdrawStatus { SUCCESS, INSUFFICIENT_BALANCE, INVALID_AMOUNT };
+
+  BankAccount(std::string name, double initialBalance)
+      : accountName(name), balance(initialBalance) {
+    accountNumber = nextAccountNumber++;
+  }
+
+  int getAccountNumber() const { return accountNumber; }
+
+  std::string getAccountName() const { return accountName; }
+
+  double getBalance() const { return balance; }
+
+  DepositStatus deposit(double amount) {
+    if (amount <= 0) return DepositStatus::INVALID_AMOUNT;
+    balance += amount;
+    return DepositStatus::SUCCESS;
+  }
+
+  WithdrawStatus withdraw(double amount) {
+    if (amount <= 0) return WithdrawStatus::INVALID_AMOUNT;
+    if (amount > balance) return WithdrawStatus::INSUFFICIENT_BALANCE;
+    balance -= amount;
+    return WithdrawStatus::SUCCESS;
+  }
+};
+
+#endif
